ddb1b9f197e5f09c307513af414cd40ba87a2836
Christian Valeri <mortlaxris@outlook.com>
2022-03-17 18:56:44 -0500

    js

---
+++ style.js
@@ -0,0 +1,9 @@
+const btn = document.querySelectorAll(".btn");
+let pScore = document.getElementById("p-score")
+let cScore = document.getElementById("c-score")
+let win = false
+let loss = false
+
+function rock() {
+
+}
