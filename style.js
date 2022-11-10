3af233a7f64af9d7e4af743f0e2f846c633bcba4
Christian Valeri <mortlaxris@outlook.com>
2022-11-10 17:15:27 -0600

    Added stuff

--- style.js
+++ style.js
@@ -1,83 +1,91 @@
 const game = () => {
-
+/***Signs***/
     const signPlayer = document.getElementById("sign-player");
     const signComputer = document.getElementById("sign-computer");
+
+    const outcome = document.getElementById("outcome-span");
+/** let playerScore = document.getElementById("p-score");
+    let cScoreboard = document.getElementById("c-score"); **/
+
+/***buttons***/
     const rockEl = document.getElementById("rock");
     const paperEl = document.getElementById("paper");
     const scissorEl = document.getElementById("scissor");
-    let btns = document.querySelectorAll('button');
-    const choices = ["rock", "paper", "scissors"];
-    let playerScore = 0;
-    let computerScore = 0;
+    const btns = document.querySelectorAll("button");
 
+    const choices = ["rock", "paper", "scissors"];
+    let playerScore= 0;
+    let computerScore = 0;
+    let randomChoices
+    let playerChoice
 
 /**************When player clicks button it will appear in " Your choice: "*************/
 
-    rockEl.addEventListener("click", function(){
-        signPlayer.textContent =  " " + choices[0];
-});
+    rockEl.addEventListener("click", () => {
+            signPlayer.textContent = " " + choices[0];
+            return playerChoice = choices[0];
+        });
 
-    paperEl.addEventListener("click", function(){
-        signPlayer.textContent = " " + choices[1];
-});
+    paperEl.addEventListener("click", () => {
+            signPlayer.textContent = " " + choices[1];
+            return playerChoice = choices[1];
+        });
 
-    scissorEl.addEventListener("click", function(){
-        signPlayer.textContent = " " + choices[2];
-});
+    scissorEl.addEventListener("click", () => {
+            signPlayer.textContent = " " + choices[2];
+            return playerChoice = choices[2];
+
+        });
     for (i of btns) {
-    i.addEventListener('click', function() {
-        randomChoices = choices[Math.floor(Math.random()*choices.length)];
-        signComputer.textContent = " " + randomChoices;
-        console.log(randomChoices);
-    });
+    i.addEventListener('click', () => {
+            let randomChoices = choices[Math.floor(Math.random() * choices.length)];
+            signComputer.textContent = " " + randomChoices;
+            console.log(randomChoices);
+            return randomChoices;
+        });
 };
+winner(randomChoices, playerChoice);
+
 /**************Code for who wins the round "*************/
 
-    const winner = (cpu,plyr) => {
-        const pScore = document.getElementById("p-score").textContent;
-        const cScore = document.getElementById("c-score").textContent;
+    const winner = (cpu, plyr) => {
+        const pScoreboard = document.getElementById("p-score").textContent;
+        const cScoreboard = document.getElementById("c-score").textContent;
         const outcome = document.getElementById("outcome").textContent;
+        cpu = cpu.toLowerCase(); 
+        plyr = plyr.toLowerCase(); 
+
         if (cpu === plyr){
             outcome.textContent = "Tie"
-        }
-        else if (cpu == "rock")
-            if (plyr == "paper"){
-            outcome.textContent = " " + "Player Won!"
-            playerScore++
-            pScore.textContent  = playerScore
-        }   else {
-            outcome.textContent = " " + "Computer Won!"
-            computerScore++
-            cScore.textContent  = computerScore
-        }
+        };
 
-        else if (cpu == "paper")
-            if (plyr == "scissor"){
-            outcome.textContent = " " + "Player Won!"
-            playerScore++
-            pScore.textContent  = playerScore
+         if (cpu === choices[0])//rock
+            if (plyr === choices[1]){//paper
+            outcome.textContent = " " + "Player Won!";
+            playerScore++;
+            pScoreboard.textContent  = playerScore;
         }   else {
-            outcome.textContent = " " + "Computer Won!"
-            computerScore++
-            cScore.textContent = computerScore
-        }
-        
-        else if (cpu == "scissor")
-            if (plyr == "paper"){
-            outcome.textContent = " " + "Player Won!"
-            playerScore++
-            pScore.textContent  = playerScore
+            outcome.textContent = " " + "Computer Won!";
+            computerScore++;
+            cScoreboard.textContent  = computerScore;
+        };
+
+         if (cpu === choices[2])//scissors
+            if (plyr === choices[0]){//rock
+            outcome.textContent = " " + "Player Won!";
+            playerScore++;
+            pScoreboard.textContent  = playerScore;
         }   else {
-            outcome.textContent = " " + "Computer Won!"
-            computerScore++
-            cScore.textContent = computerScore
-        }
+            outcome.textContent = " " + "Computer Won!";
+            computerScore++;
+            cScoreboard.textContent = computerScore;
+        };
     };
 
-    winner(signComputer.textContent, signPlayer.textContent);
+
+
+
 }
-
-
 game();
 
 
